48ac50f69b400afc7cbcd121b667fb43b4fd1a88
Jonni Rainisto <jonni.rainisto@gmail.com>
2017-10-12 14:35:34 +0300

    fb (file browser) -> fs (file selector)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # give --no-cache as parameter if you want to force rebuild
 BASE=$(pwd)
 docker build $1 -t gotek_ff -f Dockerfile.ff .
-docker build $1 -t gotek_fb -f Dockerfile.fb .
+docker build $1 -t gotek_fs -f Dockerfile.fs .
 
 docker run -v $BASE/output:/output -t gotek_ff
-docker run -v $BASE/output:/output -t gotek_fb
+docker run -v $BASE/output:/output -t gotek_fs
