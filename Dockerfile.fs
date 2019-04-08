834f6a2f4db0c6e9d970aa70b7e0344c97d75c4c
Jonni Rainisto <jonni.rainisto@gmail.com>
2019-04-08 19:49:59 +0300

    Fix file selector build and add alternative selector building

--- Dockerfile.fs
+++ Dockerfile.fs
@@ -11,7 +11,7 @@ RUN DEBIAN_FRONTEND=noninteractive apt-get -y install git make nano vim gcc-arm-
     libgmpxx4ldbl libgmp-dev libmpc3 libmpc-dev libmpfr4 libmpfr-dev libncurses5-dev python-dev
 
 RUN cd /root && git clone https://github.com/bebbo/amigaos-cross-toolchain.git && \
-    cd /root/amigaos-cross-toolchain && ./toolchain-m68k --prefix=/opt/m68k-amigaos --threads=4 build
+    cd /root/amigaos-cross-toolchain && ./toolchain-m68k --prefix=/opt/m68k-amigaos --threads=4 build || true
 
 ADD hxcfe/hxcfe /usr/bin/hxcfe
 ADD hxcfe/*.so /usr/local/lib/
--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 BASE=$(pwd)
 docker build $1 -t gotek_ff -f Dockerfile.ff .
 docker build $1 -t gotek_fs -f Dockerfile.fs .
+docker build $1 -t gotek_fs_keirf -f Dockerfile.fs.keirf .
 
 docker run -v $BASE/output:/output -t gotek_ff
 docker run -v $BASE/output:/output -t gotek_fs
+docker run -v $BASE/output_keirf:/output -t gotek_fs_keirf
